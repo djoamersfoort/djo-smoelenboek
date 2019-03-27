30754ece3347b815275de495f8f261c1d4b5b54c
Ronald Moesbergen <github@rmoesbergen.nl>
2019-03-27 19:25:19 +0100

    Use smoelenboek API

--- djo-smoelenboek.php
+++ djo-smoelenboek.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/rmoesbergen/djo-smoelenboek
  * Description: Plugin voor de DJO smoelenboek koppeling met de ledenadministratie
  * Author: Ronald Moesbergen
- * Version: 0.0.1
+ * Version: 0.1.0
  */
 
 defined('ABSPATH') or die('Go away');
@@ -28,26 +28,7 @@ if (!class_exists('DJO_Smoelenboek')) {
         'default' => ''
       );
 
-      register_setting( 'djo-smoelenboek', 'djo-smoelen-server', $options );
-      register_setting( 'djo-smoelenboek', 'djo-smoelen-username', $options );
-      register_setting( 'djo-smoelenboek', 'djo-smoelen-password', $options );
-      register_setting( 'djo-smoelenboek', 'djo-smoelen-database', $options );
-    }
-
-    private static function db_connect() {
-      if (! self::$db) {
-        try {
-          $db_host = get_option('djo-smoelen-server');
-          $db_db = get_option('djo-smoelen-database');
-          $db_user = get_option('djo-smoelen-username');
-          $db_pass = get_option('djo-smoelen-password');
-
-          self::$db = new PDO("mysql:host={$db_host};dbname={$db_db}", $db_user, $db_pass);
-        } catch (Exception $exception) {
-          return new WP_Error('admin_db_failed', $exception->getMessage());
-        }
-      }
-      return self::$db;
+      register_setting( 'djo-smoelenboek', 'djo-smoelen-url', $options );
     }
 
     public static function adminmenu() {
@@ -66,23 +47,8 @@ if (!class_exists('DJO_Smoelenboek')) {
 
       <table class="form-table">
         <tr valign="top">
-        <th scope="row">Username</th>
-        <td><input type="text" name="djo-smoelen-username" value="<?php echo esc_attr( get_option('djo-smoelen-username') ); ?>" /></td>
-        </tr>
-
-        <tr valign="top">
-        <th scope="row">Password</th>
-        <td><input type="password" name="djo-smoelen-password" value="<?php echo esc_attr( get_option('djo-smoelen-password') ); ?>" /></td>
-        </tr>
-
-        <tr valign="top">
-        <th scope="row">Database</th>
-        <td><input type="text" name="djo-smoelen-database" value="<?php echo esc_attr( get_option('djo-smoelen-database') ); ?>" /></td>
-        </tr>
-
-        <tr valign="top">
-        <th scope="row">Server</th>
-        <td><input type="text" name="djo-smoelen-server" value="<?php echo esc_attr( get_option('djo-smoelen-server') ); ?>" /></td>
+        <th scope="row">Ledenadministratie Smoelenboek API URL</th>
+        <td><input type="text" name="djo-smoelen-url" value="<?php echo esc_attr( get_option('djo-smoelen-url') ); ?>" /></td>
         </tr>
       </table>
     <?php
@@ -92,35 +58,25 @@ if (!class_exists('DJO_Smoelenboek')) {
     }
 
     public static function smoelenboek($args) {
-      $params = shortcode_atts(array('dag' => 'vr'), $args);
+      $params = shortcode_atts(array('dag' => 'vrijdag'), $args);
       $dag = $params['dag'];
 
-      $db = DJO_Smoelenboek::db_connect();
-      if (is_wp_error($db)) return $db->get_error_message();
-      $query = "SELECT contact.id, contact.voornaam, dagdeel.dag " .
-               "FROM contact " .
-               "INNER JOIN contact_dagdeel cd " .
-               "INNER JOIN dagdeel ".
-               "ON (cd.contact_id = contact.id AND cd.dagdeel_id = dagdeel.id)" .
-               "WHERE contact.eind_datum IS NULL ".
-               "AND dagdeel.eind_datum IS NULL ".
-               "AND dagdeel.dag = ? ".
-               "ORDER by FIELD(type, 'bestuur', 'begeleider', 'aspirant_begeleider', 'senior', 'lid', 'lid,strippenkaart')";
+      $idp_access_token = get_user_meta(get_current_user_id(), 'woi_idp_access_token', true);
+      $options = array('headers' => array('Authorization' => "IDP $idp_access_token"));
+      $url = get_option('djo-smoelen-url');
+      $response = wp_remote_get("$url/$dag/", $options);
 
-      $stmt = $db->prepare($query);
-      $results = $stmt->execute([$dag]);
+      if (is_wp_error($response)) return "Error receiving smoelenboek response: " . $response->get_error_message();
+      $json = wp_remote_retrieve_body($response);
+      $smoelenboek = json_decode($json);
+
       $output = "<div id='gallery-1' class='gallery gallery-columns-4 gallery-size-thumbnail'>\n";
       $counter = 1;
-      while ($row = $stmt->fetch()) {
-        $id = $row['id'];
-        $voornaam = utf8_encode($row['voornaam']);
+      foreach ($smoelenboek->{$dag} as $row) {
+        $id = $row->id;
+        $voornaam = $row->first_name;
+        $imgurl = $row->photo;
 
-        $is_present = wp_remote_get('https://admin.djoamersfoort.nl/thumb.php?id='.$id);
-        if (!is_wp_error($is_present) && $is_present['body'] == '1') {
-          $imgurl = "https:\/\/admin.djoamersfoort.nl\/images\/contacten\/thumbs\/$id.jpg";
-        } else {
-          $imgurl = '/wp-content/plugins/djo-smoelenboek/mm.png';
-        }
         $output .= "<dl class='gallery-item'>\n";
         $output .= "<dt class='gallery-icon portrait'>\n";
         $output .= "<img width='100' height='150' src='$imgurl' class='attachment-thumbnail size-thumbnail' alt='' aria-describedby='gallery-1-$id' />\n";
